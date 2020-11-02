2d5854b7695e99cd92d718d1cc6c4a1e8f2f429b
afiae <afiaekra@gmail.com>
2020-11-02 14:15:14 -0600

    update routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TravelComponent } from './travel/travel.component';
+import { AboutComponent } from './about/about.component';
 
+const routes: Routes = [
+  {path: 'travel', component: TravelComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    TravelComponent
+    TravelComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
